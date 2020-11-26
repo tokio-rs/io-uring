5278440b8c6350bacd26865cd63d87385b813669
David Kellum <dek-oss@gravitext.com>
2020-11-26 06:23:11 -0800

    derive Debug, Clone, and Copy for types Fixed and Fd (#41)

--- src/opcode.rs
+++ src/opcode.rs
@@ -68,9 +68,11 @@ pub mod types {
         _priv: (),
     }
 
+    #[derive(Debug, Clone, Copy)]
     #[repr(transparent)]
     pub struct Fd(pub RawFd);
 
+    #[derive(Debug, Clone, Copy)]
     #[repr(transparent)]
     pub struct Fixed(pub u32);
 
