0d987e19fb921048732db852a01e93a7d62a7e56
Gardner Vickers <gardner@vickers.me>
2023-03-25 13:01:25 -0400

    Probe for SendZc support to ensure we are on 6.0 for the test

--- io-uring-test/src/tests/register_sync_cancel.rs
+++ io-uring-test/src/tests/register_sync_cancel.rs
@@ -82,8 +82,8 @@ pub fn test_register_sync_cancel_unsubmitted<S: squeue::EntryMarker, C: cqueue::
     test: &Test,
 ) -> io::Result<()> {
     require!(
-        test; // We need at least 5.19 for these tests, UringCmd16 is a proxy for that.
-        test.probe.is_supported(opcode::UringCmd16::CODE);
+        test; // We need at least 6.0 to use `IORING_REGISTER_SYNC_CANCEL`. opcode::SendZc is a proxy for that requirement.
+        test.probe.is_supported(opcode::SendZc::CODE);
     );
     // Test that we can cancel operations which have not yet been submitted.
     const USER_DATA: u64 = 42u64;
