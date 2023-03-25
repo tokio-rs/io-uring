259b6444718c5da719b04755d00dfc60a5317679
Frank Rehwinkel <frankrehwinkel@gmail.com>
2023-03-25 08:35:26 -0400

    move comments
    And restore old comment.

--- src/types.rs
+++ src/types.rs
@@ -77,6 +77,16 @@ pub struct Fixed(pub u32);
 
 bitflags! {
     /// Options for [`Timeout`](super::Timeout).
+    ///
+    /// The default behavior is to treat the timespec as a relative time interval. `flags` may
+    /// contain [`types::TimeoutFlags::ABS`] to indicate the timespec represents an absolute
+    /// time. When an absolute time is being specified, the kernel will use its monotonic clock
+    /// unless one of the following flags is set (they may not both be set):
+    /// [`types::TimeoutFlags::BOOTTIME`] or [`types::TimeoutFlags::REALTIME`].
+    ///
+    /// The default behavior when the timeout expires is to return a CQE with -libc::ETIME in
+    /// the res field. To change this behavior to have zero returned, include
+    /// [`types::TimeoutFlags::ETIME_SUCCESS`].
     pub struct TimeoutFlags: u32 {
         const ABS = sys::IORING_TIMEOUT_ABS;
 
