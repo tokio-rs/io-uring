tokio-rs/io-uring

commit 1e5127a59d8aaa8fbc5d8c21ce3e50490af56fc5

Improve docs (#47)
* Improve docs

* Allow ? in Probe doctest

* Don't test register_probe on CI

* Update documentation of cqueue::Entry::result

* Mention that Fixed can reduce overhead

src/opcode.rs
@@ -1,4 +1,4 @@
-//! Operation code
+//! Operation codes that can be used to construct [`squeue::Entry`](crate::squeue::Entry)s.
 
 #![allow(clippy::new_without_default)]
 
@@ -48,46 +48,56 @@ mod sealed {
     }
 }
 
+/// Common Linux types not provided by libc.
 pub mod types {
     use crate::sys;
     use bitflags::bitflags;
     use std::os::unix::io::RawFd;
 
     pub use sys::__kernel_rwf_t as RwFlags;
+
     pub use sys::__kernel_timespec as Timespec;
 
-    /// Opaque types, you should use `libc::statx` instead.
+    /// Opaque types, you should use [`statx`](struct@libc::statx) instead.
     #[repr(C)]
     pub struct statx {
         _priv: (),
     }
 
-    /// Opaque types, you should use `libc::epoll_event` instead.
+    /// Opaque types, you should use [`epoll_event`](libc::epoll_event) instead.
     #[repr(C)]
     pub struct epoll_event {
         _priv: (),
     }
 
+    /// A file descriptor that has not been registered with io_uring.
     #[derive(Debug, Clone, Copy)]
     #[repr(transparent)]
     pub struct Fd(pub RawFd);
 
+    /// A file descriptor that has been registered with io_uring using
+    /// [`Submitter::register_files`](crate::Submitter::register_files). This can reduce overhead
+    /// compared to using [`Fd`] in some cases.
     #[derive(Debug, Clone, Copy)]
     #[repr(transparent)]
     pub struct Fixed(pub u32);
 
     bitflags! {
+        /// Options for [`Timeout`](super::Timeout).
         pub struct TimeoutFlags: u32 {
             const ABS = sys::IORING_TIMEOUT_ABS;
         }
     }
 
     bitflags! {
+        /// Options for [`Fsync`](super::Fsync).
         pub struct FsyncFlags: u32 {
             const DATASYNC = sys::IORING_FSYNC_DATASYNC;
         }
     }
 
+    /// Wrapper around `open_how` as used in [the `openat2(2)` system
+    /// call](https://man7.org/linux/man-pages/man2/openat2.2.html).
     #[derive(Default, Debug, Clone, Copy)]
     #[repr(transparent)]
     pub struct OpenHow(sys::open_how);
@@ -180,6 +190,9 @@ macro_rules! opcode {
                 }
             }
 
+            /// The opcode of the operation. This can be passed to
+            /// [`Probe::is_supported`](crate::Probe::is_supported) to check if this operation is
+            /// supported with the current kernel.
             pub const CODE: u8 = $opcode as _;
 
             $(
@@ -224,9 +237,7 @@ opcode!(
 );
 
 opcode!(
-    /// Vectored read operations, similar to `preadv2 (2)`.
-    ///
-    /// The return values match those documented in the `preadv2 (2)` man pages.
+    /// Vectored read, equivalent to `preadv2(2)`.
     #[derive(Debug)]
     pub struct Readv {
         fd: { impl sealed::UseFixed },
@@ -236,7 +247,7 @@ opcode!(
         ioprio: u16 = 0,
         offset: libc::off_t = 0,
         /// specified for read operations, contains a bitwise OR of per-I/O flags,
-        /// as described in the `preadv2 (2)` man page.
+        /// as described in the `preadv2(2)` man page.
         rw_flags: types::RwFlags = 0
     }
 
@@ -262,9 +273,7 @@ opcode!(
 );
 
 opcode!(
-    /// Vectored write operations, similar to `pwritev2 (2)`.
-    ///
-    /// The return values match those documented in the `pwritev2 (2)` man pages.
+    /// Vectored write, equivalent to `pwritev2(2)`.
     #[derive(Debug)]
     pub struct Writev {
         fd: { impl sealed::UseFixed },
@@ -274,7 +283,7 @@ opcode!(
         ioprio: u16 = 0,
         offset: libc::off_t = 0,
         /// specified for write operations, contains a bitwise OR of per-I/O flags,
-        /// as described in the `preadv2 (2)` man page.
+        /// as described in the `preadv2(2)` man page.
         rw_flags: types::RwFlags = 0
     }
 
@@ -300,17 +309,21 @@ opcode!(
 );
 
 opcode!(
-    /// File sync. See also `fsync (2)`.
+    /// File sync, equivalent to `fsync(2)`.
     ///
-    /// Note that, while I/O is initiated in the order in which it appears in the submission queue, completions are unordered.
-    /// For example, an application which places a write I/O followed by an fsync in the submission queue cannot expect the fsync to apply to the write. The two operations execute in parallel, so the fsync may complete before the write is issued to the storage. The same is also true for previously issued writes that have not completed prior to the fsync.
+    /// Note that, while I/O is initiated in the order in which it appears in the submission queue,
+    /// completions are unordered. For example, an application which places a write I/O followed by
+    /// an fsync in the submission queue cannot expect the fsync to apply to the write. The two
+    /// operations execute in parallel, so the fsync may complete before the write is issued to the
+    /// storage. The same is also true for previously issued writes that have not completed prior to
+    /// the fsync.
     #[derive(Debug)]
     pub struct Fsync {
         fd: { impl sealed::UseFixed },
         ;;
         /// The `flags` bit mask may contain either 0, for a normal file integrity sync,
         /// or [types::FsyncFlags::DATASYNC] to provide data sync only semantics.
-        /// See the descriptions of `O_SYNC` and `O_DSYNC` in the `open (2)` manual page for more information.
+        /// See the descriptions of `O_SYNC` and `O_DSYNC` in the `open(2)` manual page for more information.
         flags: types::FsyncFlags = types::FsyncFlags::empty()
     }
 
@@ -328,9 +341,10 @@ opcode!(
 );
 
 opcode!(
-    /// Read from pre-mapped buffers.
+    /// Read from pre-mapped buffers that have been previously registered with
+    /// [`Submitter::register_buffers`](crate::Submitter::register_buffers).
     ///
-    /// The return values match those documented in the `preadv2 (2)` man pages.
+    /// The return values match those documented in the `preadv2(2)` man pages.
     #[derive(Debug)]
     pub struct ReadFixed {
         /// The `buf_index` is an index into an array of fixed buffers,
@@ -343,7 +357,7 @@ opcode!(
         offset: libc::off_t = 0,
         ioprio: u16 = 0,
         /// specified for read operations, contains a bitwise OR of per-I/O flags,
-        /// as described in the `preadv2 (2)` man page.
+        /// as described in the `preadv2(2)` man page.
         rw_flags: types::RwFlags = 0
     }
 
@@ -371,9 +385,10 @@ opcode!(
 );
 
 opcode!(
-    /// Write to pre-mapped buffers.
+    /// Write to pre-mapped buffers that have been previously registered with
+    /// [`Submitter::register_buffers`](crate::Submitter::register_buffers).
     ///
-    /// The return values match those documented in the `pwritev2 (2)` man pages.
+    /// The return values match those documented in the `pwritev2(2)` man pages.
     #[derive(Debug)]
     pub struct WriteFixed {
         /// The `buf_index` is an index into an array of fixed buffers,
@@ -386,7 +401,7 @@ opcode!(
         ioprio: u16 = 0,
         offset: libc::off_t = 0,
         /// specified for write operations, contains a bitwise OR of per-I/O flags,
-        /// as described in the `preadv2 (2)` man page.
+        /// as described in the `preadv2(2)` man page.
         rw_flags: types::RwFlags = 0
     }
 
@@ -421,7 +436,7 @@ opcode!(
     #[derive(Debug)]
     pub struct PollAdd {
         /// The bits that may be set in `flags` are defined in `<poll.h>`,
-        /// and documented in `poll (2)`.
+        /// and documented in `poll(2)`.
         fd: { impl sealed::UseFixed },
         flags: { u32 }
         ;;
@@ -452,7 +467,7 @@ opcode!(
 );
 
 opcode!(
-    /// Remove an existing poll request.
+    /// Remove an existing [poll](PollAdd) request.
     ///
     /// If found, the `result` method of the `cqueue::Entry` will return 0.
     /// If not found, `result` will return `-libc::ENOENT`.
@@ -476,9 +491,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `sync_file_range (2)` on the file descriptor.
-    ///
-    /// See also `sync_file_range (2)`. for the general description of the related system call.
+    /// Sync a file segment with disk, equivalent to `sync_file_range(2)`.
     #[derive(Debug)]
     pub struct SyncFileRange {
         fd: { impl sealed::UseFixed },
@@ -510,11 +523,10 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `sendmsg (2)` system call.
+    /// Send a message on a socket, equivalent to `send(2)`.
     ///
-    /// fd must be set to the socket file descriptor, addr must contains a pointer to the msghdr structure,
-    /// and flags holds the flags associated with the system call.
-    /// See also `sendmsg (2)`. for the general description of the related system call.
+    /// fd must be set to the socket file descriptor, addr must contains a pointer to the msghdr
+    /// structure, and flags holds the flags associated with the system call.
     #[derive(Debug)]
     pub struct SendMsg {
         fd: { impl sealed::UseFixed },
@@ -541,9 +553,9 @@ opcode!(
 );
 
 opcode!(
-    /// Works just like [SendMsg], except for instead.
+    /// Receive a message on a socket, equivalent to `recvmsg(2)`.
     ///
-    /// See the description of [SendMsg].
+    /// See also the description of [`SendMsg`].
     #[derive(Debug)]
     pub struct RecvMsg {
         fd: { impl sealed::UseFixed },
@@ -570,7 +582,7 @@ opcode!(
 );
 
 opcode!(
-    /// This command will register a timeout operation.
+    /// Register a timeout operation.
     ///
     /// A timeout will trigger a wakeup event on the completion ring for anyone waiting for events.
     /// A timeout condition is met when either the specified timeout expires, or the specified number of events have completed.
@@ -585,7 +597,7 @@ opcode!(
         /// `count` may contain a completion event count.
         count: u32 = 0,
 
-        /// `flags` may contain [types::TimeoutFlags::ABS] for an absolutel timeout value, or 0 for a relative timeout.
+        /// `flags` may contain [types::TimeoutFlags::ABS] for an absolute timeout value, or 0 for a relative timeout.
         flags: types::TimeoutFlags = types::TimeoutFlags::empty()
     }
 
@@ -608,7 +620,7 @@ opcode!(
 // === 5.5 ===
 
 opcode!(
-    /// Attempt to remove an existing timeout operation.
+    /// Attempt to remove an existing [timeout operation](Timeout).
     pub struct TimeoutRemove {
         user_data: { u64 },
         ;;
@@ -630,7 +642,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of an `accept4 (2)` system call.
+    /// Accept a new connection on a socket, equivalent to `accept4(2)`.
     pub struct Accept {
         fd: { impl sealed::UseFixed },
         addr: { *mut libc::sockaddr },
@@ -677,9 +689,9 @@ opcode!(
 );
 
 opcode!(
-    /// This request must be linked with
-    /// another request through [Flags::IO_LINK](crate::squeue::Flags::IO_LINK) which is described below.
-    /// Unlike [Timeout], [LinkTimeout] acts on the linked request, not the completion queue.
+    /// This request must be linked with another request through
+    /// [`Flags::IO_LINK`](crate::squeue::Flags::IO_LINK) which is described below.
+    /// Unlike [`Timeout`], [`LinkTimeout`] acts on the linked request, not the completion queue.
     pub struct LinkTimeout {
         timespec: { *const types::Timespec },
         ;;
@@ -702,7 +714,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `connect (2)` system call.
+    /// Connect a socket, equivalent to `connect(2)`.
     pub struct Connect {
         fd: { impl sealed::UseFixed },
         addr: { *const libc::sockaddr },
@@ -727,7 +739,7 @@ opcode!(
 // === 5.6 ===
 
 opcode!(
-    /// Issue the equivalent of a `fallocate(2)` system call.
+    /// Preallocate or deallocate space to a file, equivalent to `fallocate(2)`.
     pub struct Fallocate {
         fd: { impl sealed::UseFixed },
         len: { libc::off_t },
@@ -752,7 +764,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `openat(2)` system call.
+    /// Open a file, equivalent to `openat(2)`.
     pub struct Openat {
         dirfd: { impl sealed::UseFd },
         pathname: { *const libc::c_char },
@@ -777,7 +789,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `close(2)` system call.
+    /// Close a file descriptor, equivalent to `close(2)`.
     pub struct Close {
         fd: { impl sealed::UseFd }
         ;;
@@ -796,8 +808,9 @@ opcode!(
 );
 
 opcode!(
-    /// This command is an alternative to using [crate::Submitter::register_files_update]
-    /// which then works in an async fashion, like the rest of the io_uring commands.
+    /// This command is an alternative to using
+    /// [`Submitter::register_files_update`](crate::Submitter::register_files_update) which then
+    /// works in an async fashion, like the rest of the io_uring commands.
     pub struct FilesUpdate {
         fds: { *const RawFd },
         len: { u32 },
@@ -821,7 +834,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `statx(2)` system call.
+    /// Get file status, equivalent to `statx(2)`.
     pub struct Statx {
         dirfd: { impl sealed::UseFd },
         pathname: { *const libc::c_char },
@@ -851,7 +864,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `read(2)` system call.
+    /// Read from a file descriptor, equivalent to `read(2)`.
     pub struct Read {
         fd: { impl sealed::UseFixed },
         buf: { *mut u8 },
@@ -887,7 +900,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `write(2)` system call.
+    /// Write to a file descriptor, equivalent to `write(2)`.
     pub struct Write {
         fd: { impl sealed::UseFixed },
         buf: { *const u8 },
@@ -920,7 +933,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `posix_fadvise(2)` system call.
+    /// Predeclare an access pattern for file data, equivalent to `posix_fadvise(2)`.
     pub struct Fadvise {
         fd: { impl sealed::UseFixed },
         len: { libc::off_t },
@@ -945,7 +958,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `madvise(2)` system call.
+    /// Give advice about use of memory, equivalent to `madvise(2)`.
     pub struct Madvise {
         addr: { *const libc::c_void },
         len: { libc::off_t },
@@ -969,7 +982,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `send(2)` system call.
+    /// Send a message on a socket, equivalent to `send(2)`.
     pub struct Send {
         fd: { impl sealed::UseFixed },
         buf: { *const u8 },
@@ -994,7 +1007,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `recv(2) system call.
+    /// Receive a message from a socket, equivalent to `recv(2)`.
     pub struct Recv {
         fd: { impl sealed::UseFixed },
         buf: { *mut u8 },
@@ -1021,7 +1034,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `openat2(2) system call.
+    /// Open a file, equivalent to `openat2(2)`.
     pub struct Openat2 {
         dirfd: { impl sealed::UseFd },
         pathname: { *const libc::c_char },
@@ -1045,7 +1058,7 @@ opcode!(
 );
 
 opcode!(
-    /// Issue the equivalent of a `epoll_ctl(2) system call.
+    /// Modify an epoll file descriptor, equivalent to `epoll_ctl(2)`.
     pub struct EpollCtl {
         epfd: { impl sealed::UseFixed },
         fd: { impl sealed::UseFd },
@@ -1073,6 +1086,9 @@ opcode!(
 
 #[cfg(feature = "unstable")]
 opcode!(
+    /// Splice data to/from a pipe, equivalent to `splice(2)`.
+    ///
+    /// Requires the `unstable` feature.
     pub struct Splice {
         fd_in: { impl sealed::UseFixed },
         off_in: { i64 },
@@ -1110,6 +1126,11 @@ opcode!(
 
 #[cfg(feature = "unstable")]
 opcode!(
+    /// Register `nbufs` buffers that each have the length `len` with ids starting from `big` in the
+    /// group `bgid` that can be used for any request. See
+    /// [`BUFFER_SELECT`](crate::squeue::Flags::BUFFER_SELECT) for more info.
+    ///
+    /// Requires the `unstable` feature.
     pub struct ProvideBuffers {
         addr: { *mut u8 },
         len: { i32 },
@@ -1137,6 +1158,10 @@ opcode!(
 
 #[cfg(feature = "unstable")]
 opcode!(
+    /// Remove some number of buffers from a buffer group. See
+    /// [`BUFFER_SELECT`](crate::squeue::Flags::BUFFER_SELECT) for more info.
+    ///
+    /// Requires the `unstable` feature.
     pub struct RemoveBuffers {
         nbufs: { u16 },
         bgid: { u16 }
@@ -1160,6 +1185,9 @@ opcode!(
 
 #[cfg(feature = "unstable")]
 opcode!(
+    /// Duplicate pipe content, equivalent to `tee(2)`.
+    ///
+    /// Requires the `unstable` feature.
     pub struct Tee {
         fd_in: { impl sealed::UseFixed },
         fd_out: { impl sealed::UseFixed },

src/concurrent/cqueue.rs
@@ -2,6 +2,8 @@ use std::sync::atomic;
 
 use crate::cqueue::{self, Entry};
 
+/// The completion queue of a concurrent io_uring instance. This stores all the I/O operations that
+/// have completed.
 pub struct CompletionQueue<'a> {
     pub(crate) queue: &'a cqueue::CompletionQueue,
     pub(crate) ring_mask: u32,
@@ -9,16 +11,20 @@ pub struct CompletionQueue<'a> {
 }
 
 impl CompletionQueue<'_> {
+    /// If queue is full and [`is_feature_nodrop`](crate::Parameters::is_feature_nodrop) is not set,
+    /// new events may be dropped. This records the number of dropped events.
     #[inline]
     pub fn overflow(&self) -> u32 {
         self.queue.overflow()
     }
 
+    /// Get the total number of entries in the completion queue ring buffer.
     #[inline]
     pub fn capacity(&self) -> usize {
         self.ring_entries as usize
     }
 
+    /// Get the number of unread completion queue events in the ring buffer.
     #[inline]
     pub fn len(&self) -> usize {
         unsafe {
@@ -29,16 +35,22 @@ impl CompletionQueue<'_> {
         }
     }
 
+    /// Returns `true` if there are no completion queue events to be processed.
     #[inline]
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    /// Returns `true` if the completion queue is at maximum capacity. If
+    /// [`is_feature_nodrop`](crate::Parameters::is_feature_nodrop) is not set, this will cause any
+    /// new completion queue events to be dropped by the kernel.
     #[inline]
     pub fn is_full(&self) -> bool {
         self.len() == self.capacity()
     }
 
+    /// Take the oldest completion queue event out of the queue. This can be called from multiple
+    /// threads simultaneously.
     pub fn pop(&self) -> Option<Entry> {
         unsafe {
             loop {

src/concurrent/mod.rs
@@ -1,4 +1,6 @@
 //! Concurrent IoUring.
+//!
+//! This allows io_uring to be used from several threads at once.
 
 mod cqueue;
 mod squeue;
@@ -10,6 +12,8 @@ use parking_lot::Mutex;
 pub use squeue::SubmissionQueue;
 
 /// Concurrent IoUring instance
+///
+/// You can create it with [`IoUring::concurrent`](crate::IoUring::concurrent).
 pub struct IoUring {
     ring: crate::IoUring,
     push_lock: Mutex<()>,
@@ -26,7 +30,8 @@ impl IoUring {
         }
     }
 
-    /// Initiate and/or complete asynchronous I/O
+    /// Initiate and/or complete asynchronous I/O. See
+    /// [`Submitter::enter`](crate::Submitter::enter) for more details.
     ///
     /// # Safety
     ///
@@ -42,19 +47,21 @@ impl IoUring {
         self.ring.enter(to_submit, min_complete, flag, sig)
     }
 
-    /// Initiate asynchronous I/O.
+    /// Submit all queued submission queue events to the kernel.
     #[inline]
     pub fn submit(&self) -> io::Result<usize> {
         self.ring.submit()
     }
 
-    /// Initiate and/or complete asynchronous I/O
+    /// Submit all queued submission queue events to the kernel and wait for at least `want`
+    /// completion events to complete.
     #[inline]
     pub fn submit_and_wait(&self, want: usize) -> io::Result<usize> {
         self.ring.submit_and_wait(want)
     }
 
-    /// Get submission queue
+    /// Get the submission queue of the io_uring instace. This is used to send I/O requests to the
+    /// kernel.
     pub fn submission(&self) -> SubmissionQueue<'_> {
         unsafe {
             SubmissionQueue {
@@ -66,7 +73,7 @@ impl IoUring {
         }
     }
 
-    /// Get completion queue
+    /// Get completion queue. This is used to receive I/O completion events from the kernel.
     pub fn completion(&self) -> CompletionQueue<'_> {
         unsafe {
             CompletionQueue {
@@ -77,7 +84,7 @@ impl IoUring {
         }
     }
 
-    /// Get original IoUring instance
+    /// Get the original [`IoUring`](crate::IoUring) instance.
     pub fn into_inner(self) -> crate::IoUring {
         self.ring
     }

src/concurrent/squeue.rs
@@ -5,6 +5,8 @@ use crate::util::unsync_load;
 
 use parking_lot::Mutex;
 
+/// An io_uring instance's submission queue. This contains all the I/O operations the application
+/// sends to the kernel.
 pub struct SubmissionQueue<'a> {
     pub(crate) queue: &'a squeue::SubmissionQueue,
     pub(crate) push_lock: &'a Mutex<()>,
@@ -13,21 +15,27 @@ pub struct SubmissionQueue<'a> {
 }
 
 impl SubmissionQueue<'_> {
+    /// When [`is_setup_sqpoll`](crate::Parameters::is_setup_sqpoll) is set, whether the kernel
+    /// threads has gone to sleep and requires a system call to wake it up.
     #[inline]
     pub fn need_wakeup(&self) -> bool {
         self.queue.need_wakeup()
     }
 
+    /// The number of invalid submission queue entries that have been encountered in the ring
+    /// buffer.
     #[inline]
     pub fn dropped(&self) -> u32 {
         self.queue.dropped()
     }
 
+    /// Get the total number of entries in the submission queue ring buffer.
     #[inline]
     pub fn capacity(&self) -> usize {
         self.ring_entries as usize
     }
 
+    /// Get the number of submission queue events in the ring buffer.
     #[inline]
     pub fn len(&self) -> usize {
         unsafe {
@@ -38,23 +46,26 @@ impl SubmissionQueue<'_> {
         }
     }
 
+    /// Returns `true` if the submission queue ring buffer is empty.
     #[inline]
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    /// Returns `true` if the submission queue ring buffer has reached capacity, and no more events
+    /// can be added before the kernel consumes some.
     #[inline]
     pub fn is_full(&self) -> bool {
         self.len() == self.capacity()
     }
 
-    /// Attempts to push an [Entry] into the queue.
+    /// Attempts to push an [`Entry`] into the queue.
     /// If the queue is full, the element is returned back as an error.
     ///
     /// # Safety
     ///
-    /// Developers must ensure that parameters of the [Entry] (such as buffer) are valid,
-    /// otherwise it may cause memory problems.
+    /// Developers must ensure that parameters of the [`Entry`] (such as buffer) are valid and will
+    /// be valid for the entire duration of the operation, otherwise it may cause memory problems.
     pub unsafe fn push(&self, Entry(entry): Entry) -> Result<(), Entry> {
         let _lock = self.push_lock.lock();
 

src/cqueue.rs
@@ -5,6 +5,7 @@ use std::sync::atomic;
 use crate::sys;
 use crate::util::{unsync_load, Mmap};
 
+/// An io_uring instance's completion queue. This stores all the I/O operations that have completed.
 pub struct CompletionQueue {
     pub(crate) head: *const atomic::AtomicU32,
     pub(crate) tail: *const atomic::AtomicU32,
@@ -19,11 +20,12 @@ pub struct CompletionQueue {
     flags: *const atomic::AtomicU32,
 }
 
-/// Completion Entry
+/// An entry in the completion queue, representing a complete I/O operation.
 #[repr(transparent)]
 #[derive(Clone)]
 pub struct Entry(pub(crate) sys::io_uring_cqe);
 
+/// A snapshot of the completion queue.
 pub struct AvailableQueue<'a> {
     head: u32,
     tail: u32,
@@ -56,12 +58,17 @@ impl CompletionQueue {
         }
     }
 
-    /// If queue is full, the new event maybe dropped.
-    /// This value records number of dropped events.
+    /// If queue is full and [`is_feature_nodrop`](crate::Parameters::is_feature_nodrop) is not set,
+    /// new events may be dropped. This records the number of dropped events.
     pub fn overflow(&self) -> u32 {
         unsafe { (*self.overflow).load(atomic::Ordering::Acquire) }
     }
 
+    /// Whether eventfd notifications are disabled when a request is completed and queued to the CQ
+    /// ring. This library currently does not provide a way to set it, so this will always be
+    /// `false`.
+    ///
+    /// Requires the `unstable` feature.
     #[cfg(feature = "unstable")]
     pub fn eventfd_disabled(&self) -> bool {
         unsafe {
@@ -69,11 +76,13 @@ impl CompletionQueue {
         }
     }
 
+    /// Get the total number of entries in the completion queue ring buffer.
     #[inline]
     pub fn capacity(&self) -> usize {
         unsafe { self.ring_entries.read() as usize }
     }
 
+    /// Get the number of unread completion queue events in the ring buffer.
     #[inline]
     pub fn len(&self) -> usize {
         unsafe {
@@ -84,17 +93,21 @@ impl CompletionQueue {
         }
     }
 
+    /// Returns `true` if there are no completion queue events to be processed.
     #[inline]
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    /// Returns `true` if the completion queue is at maximum capacity. If
+    /// [`is_feature_nodrop`](crate::Parameters::is_feature_nodrop) is not set, this will cause any
+    /// new completion queue events to be dropped by the kernel.
     #[inline]
     pub fn is_full(&self) -> bool {
         self.len() == self.capacity()
     }
 
-    /// Get currently available completion queue
+    /// Take a snapshot of the completion queue.
     pub fn available(&mut self) -> AvailableQueue<'_> {
         unsafe {
             AvailableQueue {
@@ -109,7 +122,7 @@ impl CompletionQueue {
 }
 
 impl AvailableQueue<'_> {
-    /// Sync queue
+    /// Synchronize this snapshot with the real queue.
     pub fn sync(&mut self) {
         unsafe {
             (*self.queue.head).store(self.head, atomic::Ordering::Release);
@@ -117,11 +130,15 @@ impl AvailableQueue<'_> {
         }
     }
 
+    /// Get the total number of entries in the completion queue ring buffer.
     #[inline]
     pub fn capacity(&self) -> usize {
         self.ring_entries as usize
     }
 
+    /// Returns `true` if the completion queue is at maximum capacity. If
+    /// [`is_feature_nodrop`](crate::Parameters::is_feature_nodrop) is not set, this will cause any
+    /// new completion queue events to be dropped by the kernel.
     #[inline]
     pub fn is_full(&self) -> bool {
         self.len() == self.capacity()
@@ -160,19 +177,23 @@ impl Drop for AvailableQueue<'_> {
 }
 
 impl Entry {
-    /// Result value
+    /// The operation-specific result code. For example, for a [`Read`](crate::opcode::Read)
+    /// operation this is equivalent to the return value of the `read(2)` system call.
     pub fn result(&self) -> i32 {
         self.0.res
     }
 
-    /// User Data
-    ///
-    /// See [Entry::user_data](crate::squeue::Entry::user_data).
+    /// The user data of the request, as set by
+    /// [`Entry::user_data`](crate::squeue::Entry::user_data) on the submission queue event.
     pub fn user_data(&self) -> u64 {
         self.0.user_data
     }
 
-    /// Flags
+    /// Metadata related to the operation.
+    ///
+    /// This is currently used for:
+    /// - Storing the selected buffer ID, if one was selected. See
+    /// [`BUFFER_SELECT`](crate::squeue::Flags::BUFFER_SELECT) for more info.
     pub fn flags(&self) -> u32 {
         self.0.flags
     }

commit 53d6dfbd9cc6259772eeb5d329387ecfe4cd5d59

opcode: fix doc typo in provide buffers
Replace the provide buffers opcode parameter 'big' with 'bid' (stands
for 'buffer id') in documentation comments.

Fixes: 1e5127a59d8a ("Improve docs (#47)")
Signed-off-by: Adrien Vasseur <git@t3ssel.com>

src/opcode.rs
@@ -1186,7 +1186,7 @@ opcode! {
 }
 
 opcode! {
-    /// Register `nbufs` buffers that each have the length `len` with ids starting from `big` in the
+    /// Register `nbufs` buffers that each have the length `len` with ids starting from `bid` in the
     /// group `bgid` that can be used for any request. See
     /// [`BUFFER_SELECT`](crate::squeue::Flags::BUFFER_SELECT) for more info.
     pub struct ProvideBuffers {