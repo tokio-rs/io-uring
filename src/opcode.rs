363135d34e411c7b99a4b0e2252497142b3955a9
bits0rcerer <25325997+bits0rcerer@users.noreply.github.com>
2023-12-21 22:35:24 +0100

    pass flags in msg ring op code

--- src/opcode.rs
+++ src/opcode.rs
@@ -1454,8 +1454,8 @@ opcode! {
         sqe.__bindgen_anon_1.off = user_data;
         sqe.__bindgen_anon_3.msg_ring_flags = opcode_flags;
         if let Some(_flags) = user_flags {
-            // TODO(lucab): add IORING_MSG_RING_FLAGS_PASS support (in v6.3):
-            // https://lore.kernel.org/all/20230103160507.617416-1-leitao@debian.org/t/#u
+            sqe.__bindgen_anon_5.file_index = _flags;
+            unsafe {sqe.__bindgen_anon_3.msg_ring_flags |= sys::IORING_MSG_RING_FLAGS_PASS};
         }
         Entry(sqe)
     }
