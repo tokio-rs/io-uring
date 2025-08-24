591a16bb25a1718a2df70810349f0384d6044742
Motoyuki Kimura <moymoymox@gmail.com>
2025-08-24 09:57:55 +0900

    Update sys.rs to use prebuilt binding

--- src/sys/sys.rs
+++ src/sys/sys.rs
@@ -3,6 +3,12 @@ cfg_if::cfg_if! {
         include!("sys_x86_64.rs");
     } else if #[cfg(target_arch = "aarch64")] {
         include!("sys_aarch64.rs");
+    } else if #[cfg(target_arch = "riscv64")] {
+        include!("sys_riscv64.rs");
+    } else if #[cfg(target_arch = "powerpc64")] {
+        include!("sys_powerpc64.rs");
+    } else if #[cfg(target_arch = "loongarch64")] {
+        include!("sys_loongarch64.rs");
     } else {
         include!("sys_x86_64.rs");
     }
