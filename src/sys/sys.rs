841cde6e905f57bc67f0a1ec365664cdce3edffc
Darin Morrison <darinmorrison@gmail.com>
2025-09-01 13:18:17 -0600

    Add sys files for remaining tier 2 targets

--- src/sys/sys.rs
+++ src/sys/sys.rs
@@ -1,14 +1,22 @@
 cfg_if::cfg_if! {
     if #[cfg(target_arch = "x86_64")] {
         include!("sys_x86_64.rs");
+    } else if #[cfg(target_arch = "x86")] {
+        include!("sys_x86.rs");
+    } else if #[cfg(target_arch = "arm")] {
+        include!("sys_arm.rs");
     } else if #[cfg(target_arch = "aarch64")] {
         include!("sys_aarch64.rs");
     } else if #[cfg(target_arch = "riscv64")] {
         include!("sys_riscv64.rs");
+    } else if #[cfg(target_arch = "powerpc")] {
+        include!("sys_powerpc.rs");
     } else if #[cfg(target_arch = "powerpc64")] {
         include!("sys_powerpc64.rs");
     } else if #[cfg(target_arch = "loongarch64")] {
         include!("sys_loongarch64.rs");
+    } else if #[cfg(target_arch = "s390x")] {
+        include!("sys_s390x.rs");
     } else {
         include!("sys_x86_64.rs");
     }
